3124b42569fe4b94058cb4ad1e209ce50f560200
Marcelo-Diament <marcelodiament@gmail.com>
2022-05-02 21:41:55 -0300

    feat: Resolução desafio 04 mais views

---
+++ 04-sistema-de-roteamento/app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const seriesRouter = require("./routes/series")
+const app = express()
+const port = 3000
+
+app.use("/series", seriesRouter)
+
+app.listen(port, () => console.log(`Servidor rodando com sucesso na porta ${port}`))
