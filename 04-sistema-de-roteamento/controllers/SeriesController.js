44801bafe05bd4efaadd91cf7114daef749540ed
Marcelo-Diament <marcelodiament@gmail.com>
2022-05-02 21:51:39 -0300

    feat: Ajustes de espaçamento

--- 04-sistema-de-roteamento/controllers/SeriesController.js
+++ 04-sistema-de-roteamento/controllers/SeriesController.js
@@ -2,8 +2,11 @@ const series = require("../models/series")
 const path = require("path")
 
 const SeriesController = {
+
   index: (req, res) => res.sendFile(path.join(__dirname, "..", "views", "series.html")),
+
   show: (req, res) => res.send(series.filter(serie => serie.id == req.params.id)[0])
+
 };
 
 module.exports = SeriesController;
--- 04-sistema-de-roteamento/routes/series.js
+++ 04-sistema-de-roteamento/routes/series.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const SeriesController = require('../controllers/SeriesController')
 
 router.get('/:id', SeriesController.show)
+
 router.get('/', SeriesController.index)
 
 module.exports = router
